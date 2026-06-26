b4217a1b473947ea278866cc4b2b311f9b897c17
liudejin <liudejin@kis2.cn>
2026-06-26 14:59:26 +0800

    feat: 新增员工功能实现

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
  */
@@ -15,13 +17,27 @@ public class GlobalExceptionHandler {
 
     /**
      * 捕获业务异常
+     *
      * @param ex
      * @return
      */
     @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
+    public Result exceptionHandler(BaseException ex) {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    // 新增员工 用户名重复异常处理
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException sex) {
+        String message = sex.getMessage();
+        if (message.contains("Duplicate entry")) {
+            String[] s = message.split(" ");
+            String username = s[2];
+            String msg = username + "已存在";
+            return Result.error(msg);
+        }
+        return Result.error("未知错误");
     }
 
 }
